0e1e21ee450b56804a50edf7cb7e4c2733541b99
qixianpei <38662216+qixianpei@users.noreply.github.com>
2020-03-24 17:05:36 +0800

    Add files via upload

---
+++ 方程求解.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 方程求解
+
+# In[2]:
+
+
+import math
+def    equation(a,b,c)
+        h=b*b-4*a*c
+    if h>=0:
+        x1=(-b+math.sqrth(h))/2*a
+        x2=(-b-matn.sqrth(h))/2*a
+        print('x1=%.2f'%x1,'x2=%.2f'%x2)
+    else:
+        print('方程无解')
+equation(1,2,1)
+
+
+# In[ ]:
+
+
+
+
